e054266b61f77450fb134f70d11d8399540d237a
Андрей <andrik150@rambler.ru>
2022-08-12 13:45:06 +0300

    Task10

---
+++ Task/Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+int num1 = number % 100;
+int result = num1 / 10;
+Console.WriteLine($"Втроя цифра числа {result}");
